888528b4ec46a41f1e574221592a5e99465c0a43
Pablo Brague <64508767+pablobrague@users.noreply.github.com>
2024-01-18 16:42:45 -0300

    Carga Desafio Reestructuracion

---
+++ src/dao/userDao.js
@@ -0,0 +1,42 @@
+import { usersManager } from '../models/User.js';
+import { randomUUID } from 'crypto'
+import { hasheadasSonIguales, hashear } from '../utils/criptografia.js'
+
+export class UserDAO {
+
+  static createUser = async (userData) => {
+    try {
+      userData.id= randomUUID()
+      userData.password = hashear(userData.password)
+      const user = await usersManager.create(userData)
+      return user.toObject()
+    } catch (error) {
+      throw new Error('Error creating user');
+    }
+  };
+
+  static findUserByUsername = async ({username, password}) => {
+    try {
+        const user = await usersManager.findOne({ username })
+        if (!user) { throw new Error('authentication error') }
+        if (!hasheadasSonIguales({
+          recibida: password,
+          almacenada: user.password
+        })) {
+          throw new Error('authentication error')
+        }
+        return user.toObject() 
+    } catch (error) {
+      throw new Error('Error finding user by username');
+    }
+  };
+
+  static findAllUsers = async () => {
+    try {
+      return await usersManager.find({}, { password: 0 }).lean();
+    } catch (error) {
+      throw new Error('Error finding user by username');
+    }
+  };
+
+}
